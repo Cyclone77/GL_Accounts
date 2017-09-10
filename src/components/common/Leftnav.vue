d0b545e8a606be496a5893f6a8456d3b0af9fcf2
cyclone77 <fillchar@live.com>
2017-09-10 15:11:59 +0800

    解决路由定位菜单

--- src/components/common/Leftnav.vue
+++ src/components/common/Leftnav.vue
@@ -1,29 +1,40 @@
 <template>
     <div class="nav">
-        <el-menu :router="true" default-active="/" class="el-menu-vertical-demo" @select="handleOpen">
-            <el-menu-item index="/"><i class="el-icon-document"></i>首页</el-menu-item>
-            <el-menu-item index="/credit"><i class="el-icon-date"></i>应收账款</el-menu-item>
-            <el-menu-item index="/invoice"><i class="el-icon-star-off"></i>开票申请</el-menu-item>
-            <el-menu-item index="/forinvoice"><i class="el-icon-edit"></i>代开发票</el-menu-item>
-            <el-menu-item index="/yetinvoice"><i class="el-icon-share"></i>已开发票</el-menu-item>
-            <el-menu-item index="/performance"><i class="el-icon-setting"></i>部门效绩</el-menu-item>
+        <el-menu :router="true" :default-active="defaultActive" class="el-menu-vertical-demo" @select="handleOpen">
+            <el-menu-item index="/">
+                <i class="el-icon-document"></i>首页</el-menu-item>
+            <el-menu-item index="/credit">
+                <i class="el-icon-date"></i>应收账款</el-menu-item>
+            <el-menu-item index="/invoice">
+                <i class="el-icon-star-off"></i>开票申请</el-menu-item>
+            <el-menu-item index="/forinvoice">
+                <i class="el-icon-edit"></i>代开发票</el-menu-item>
+            <el-menu-item index="/yetinvoice">
+                <i class="el-icon-share"></i>已开发票</el-menu-item>
+            <el-menu-item index="/performance">
+                <i class="el-icon-setting"></i>部门效绩</el-menu-item>
         </el-menu>
     </div>
 </template>
 
 <script>
 export default {
-  name: "Leftnav",
-  data(){
-      return {}
-  },
-  methods: {
-      handleOpen(key, keyPath) {
-        console.log(key, keyPath);
-      }
+    name: "Leftnav",
+    data() {
+        return {
+            defaultActive: "/"
+        }
+    },
+    created() {
+        this.defaultActive = this.$route.path;
+    },
+    methods: {
+        handleOpen(key, keyPath) {
+            console.log(key, keyPath);
+        }
     }
 }
 </script>
 <style scoped>
-    @import '~@/css/common/Leftnav.css'
+@import '~@/css/common/Leftnav.css'
 </style>
