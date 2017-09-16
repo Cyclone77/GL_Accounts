7666c342d6e5e6ff6110ced8a4e9013971bdc447
cyclone77 <fillchar@live.com>
2017-09-16 19:28:55 +0800

    增加404

--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,15 @@ import Router from 'vue-router'
 const _import = require('./_import_' + process.env.NODE_ENV)
 
 import Layout from '@/views/layout/Layout'
-
+import NotFoundComponent from '@/views/404'
 Vue.use(Router)
 
 export default new Router({
     mode: 'history',
     routes: [{
+        path: '*',
+        component: NotFoundComponent
+    }, {
         path: '/',
         name: '首页',
         redirect: '/home',
@@ -29,7 +32,9 @@ export default new Router({
         name: '开票申请',
         component: Layout,
         children: [
-            { path: 'index', component: _import('invoice/index'), name: '发票管理 ' }
+            { path: 'index', component: _import('invoice/index'), name: '发票管理 ' },
+            { path: 'view', component: _import('invoice/view'), name: '发票申请预览 ' },
+            { path: 'add', component: _import('invoice/add'), name: '发票申请预览 ' }
         ]
     }]
 })
